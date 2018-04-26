b5d11a345a1f49d9f555611dcc65a3e8cbcf251a
Bruno Martins <bgcngm@gmail.com>
2018-04-27 00:04:21 +0100

    mojito: List additional native libraries to expose to apps
    Change-Id: I552dc44a32221d2d3cec48e1c64ed72c470d412e

--- device.mk
+++ device.mk
@@ -43,6 +43,10 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# Additional native libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
