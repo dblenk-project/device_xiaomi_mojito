5f5e6811a8c5074b8ee39f5b0aaf6ba347dcbf8b
Arian <arian.kulmer@web.de>
2020-03-23 21:39:07 +0100

    mojito: Configure boot animation
    Change-Id: Id50971e3b28c68f5035c58aa61de2e475bd1b03a

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/mojito/mojito-vendor.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
