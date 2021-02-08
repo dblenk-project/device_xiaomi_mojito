c778cfd9ae2914b1b204fe78816ecadc0cfea0d6
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 15:00:09 +0100

    mojito: Initial proprietary files list
    * From mojito 11 RKQ1.201022.002 V12.5.4.0.RKGMIXM release-keys
    
    Change-Id: I8cda29845fa273be757c60e78622083be4a91fd8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,9 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 # Screen density
 TARGET_SCREEN_DENSITY := 440
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2021-10-01
+
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
