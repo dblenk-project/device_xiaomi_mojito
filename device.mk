24094e68400e64366981b3760a739d7430f7fc6f
sashascurtu <sasha.scurtu@gmail.com>
2021-06-24 20:20:22 +0000

    mojito: Build mock power stats HAL and label it

--- device.mk
+++ device.mk
@@ -341,7 +341,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    android.hardware.power.stats@1.0-service.mock
 
 # QTI
 PRODUCT_PACKAGES += \
