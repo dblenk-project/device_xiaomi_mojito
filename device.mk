33137de2245e2a587ee555fe17638f88e64f5707
raghavt20 <raghavtandon2000@gmail.com>
2021-09-27 02:27:49 +0530

    mojito: rootdir: Import fstab.qcom from stock

--- device.mk
+++ device.mk
@@ -165,6 +165,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fstab
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
