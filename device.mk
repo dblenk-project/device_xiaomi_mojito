f0ed4e25facd4c93a4cb2a883844ef84a7634923
raghavt20 <raghavtandon2000@gmail.com>
2021-09-27 23:13:35 +0530

    mojito: overlay: Import framework-res

--- device.mk
+++ device.mk
@@ -233,6 +233,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
