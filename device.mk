766de4cbdf7436f285e4b49615636192b171f837
ujjwalkirorimal <ujjwalkirorimal@gmail.com>
2021-10-13 20:50:01 +0530

    mojito: Update DRM/Clearkey HIDL to v1.4

--- device.mk
+++ device.mk
@@ -197,8 +197,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm@1.4-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
