22703a06dd62dc4b0cd1dbd74c46b215ed0be765
raghavt20 <raghavtandon2000@gmail.com>
2021-11-19 13:47:50 +0530

    mojito: Import mem latency specific settings
    - From https://source.codeaurora.org/quic/la/platform/vendor/qcom/steppe/tree/init.qti.dcvs.sh?h=LA.UM.9.1.r1-11600-SMxxx0.0

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -227,9 +227,30 @@ case "$soc_id" in
             echo 40 > $llccbw/polling_interval
         done
 
-    # memlat specific settings are moved to seperate file under
-    # device/target specific folder
-    setprop vendor.dcvs.prop 1
+        # Enable mem_latency governor for L3, LLCC, and DDR scaling
+        for memlat in $device/*cpu*-lat/devfreq/*cpu*-lat
+        do
+            echo "mem_latency" > $memlat/governor
+            echo 10 > $memlat/polling_interval
+            echo 400 > $memlat/mem_latency/ratio_ceil
+        done
+
+        # Gold L3 ratio ceil
+        echo 4000 > /sys/class/devfreq/soc:qcom,cpu6-cpu-l3-lat/mem_latency/ratio_ceil
+
+        # Enable cdspl3 governor for L3 cdsp nodes
+        for l3cdsp in $device/*cdsp-cdsp-l3-lat/devfreq/*cdsp-cdsp-l3-lat
+        do
+            echo "cdspl3" > $l3cdsp/governor
+        done
+
+        # Enable compute governor for gold latfloor
+        for latfloor in $device/*cpu*-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
+        do
+            echo "compute" > $latfloor/governor
+            echo 10 > $latfloor/polling_interval
+        done
+    done
 
     # cpuset parameters
     echo 0-7     > /dev/cpuset/top-app/cpus
@@ -346,9 +367,30 @@ case "$soc_id" in
             echo 0 > /sys/devices/virtual/npu/msm_npu/pwr
         done
 
-    # memlat specific settings are moved to seperate file under
-    # device/target specific folder
-    setprop vendor.dcvs.prop 1
+        # Enable mem_latency governor for L3, LLCC, and DDR scaling
+        for memlat in $device/*cpu*-lat/devfreq/*cpu*-lat
+        do
+            echo "mem_latency" > $memlat/governor
+            echo 10 > $memlat/polling_interval
+            echo 400 > $memlat/mem_latency/ratio_ceil
+        done
+
+        # Gold L3 ratio ceil
+        echo 4000 > /sys/class/devfreq/soc:qcom,cpu6-cpu-l3-lat/mem_latency/ratio_ceil
+
+        # Enable cdspl3 governor for L3 cdsp nodes
+        for l3cdsp in $device/*cdsp-cdsp-l3-lat/devfreq/*cdsp-cdsp-l3-lat
+        do
+            echo "cdspl3" > $l3cdsp/governor
+        done
+
+        # Enable compute governor for gold latfloor
+        for latfloor in $device/*cpu*-ddr-latfloor*/devfreq/*cpu-ddr-latfloor*
+        do
+            echo "compute" > $latfloor/governor
+            echo 10 > $latfloor/polling_interval
+        done
+    done
 
     # cpuset parameters
     echo 0-5 > /dev/cpuset/background/cpus
