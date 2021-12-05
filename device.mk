82d26046e72daaabe7011c657668bdb6e170664f
johnmart19 <johnivan19999@gmail.com>
2021-12-06 01:11:11 +0200

    mojito: Add missing android.hardware.media.omx
    Fixes: android.hardware.media.omx@1.0-service: Service not available yet

--- device.mk
+++ device.mk
@@ -278,12 +278,17 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service
+
+PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libarbitrarybytes \
     libc2dcolorconvert \
     libplatformconfig \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_omx.vendor
 
 PRODUCT_PACKAGES += \
     libmm-omxcore \
