50f8a546f1d5032ca834ebc32f1beae167cbd53e
boedhack99 <boedhack@gmail.com>
2022-02-11 14:14:27 +0700

    mojito: Initial device tree

---
+++ lineage_mojito.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from mojito device
+$(call inherit-product, device/xiaomi/mojito/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Boot Animation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := lineage_mojito
+PRODUCT_DEVICE := mojito
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 10
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
