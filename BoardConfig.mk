c2a9c9a95cc9d3217d1b9607589da850eff11dba
boedhack99 <boedhack@gmail.com>
2022-02-13 06:20:08 +0700

    mojito: Nuke c2 codecs manifest specifics
    - It's already available in the device manifest causing a conflict, so let's remove it

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,9 +108,7 @@ BOARD_HAVE_QCOM_FM := true
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     vendor/awaken/config/device_framework_matrix.xml
-DEVICE_MANIFEST_FILE += \
-    $(DEVICE_PATH)/configs/vintf/manifest.xml \
-    hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
 # Filesystem
