ca0b473d201afce7ff148d42a59c4169e3092490
boedhack99 <boedhack@gmail.com>
2022-03-11 02:37:05 +0700

    mojito: rootdir: Import specifics K7 camera configs - Import from MIUI 13.0.5.0.SKGMIXM
    Signed-off-by: boedhack99 <boedhack@gmail.com>

--- device.mk
+++ device.mk
@@ -253,6 +253,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom_ramdisk \
+    init.camera.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
