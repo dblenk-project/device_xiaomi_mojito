831083b9a3f6520f98c75c2bf693144db234724f
Mohammed Althaf <althafvly@gmail.com>
2022-04-06 00:27:36 +0700

    mojito: Inherit launch_with_vendor_ramdisk for vAB - mojito has vendor_boot partition
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 #
 
 # A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
