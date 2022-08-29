ae19ccf162b8ee087290068893b324d01e625732
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-29 23:59:54 +0300

    mojito: parts: Avoid unsafe boot completed receiver
    https://stackoverflow.com/questions/52628780/how-to-fix-unsafeprotectedbroadcastreceiver
    
    Reported-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Change-Id: I6e63866639c89b81b57efc2a5537c3017caa994f
    Signed-off-by: kibria5 <mdkibria687@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -32,8 +32,10 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
+        if (!intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            return;
+        }
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
