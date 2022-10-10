1248d1b1e6096b507413e7634de037763b2a800f
Zinadin Zidan <zidan44@pixelexperience.org>
2022-10-10 23:45:37 +0600

    mojito: camera: Import camxoverridesettings txt
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I34a9fc370bab422d3a340f72e37a51593572fc71

--- device.mk
+++ device.mk
@@ -124,7 +124,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libpiex_shim
+    libpiex_shim \
+    camxoverridesettings.txt
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
