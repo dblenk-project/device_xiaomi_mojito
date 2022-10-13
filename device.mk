af5a5ac8197fdb30dd85da4db63b2ffbe8970c7d
Zinadin Zidan <zidan44@pixelexperience.org>
2022-10-13 11:03:45 +0600

    mojito: Switch to prebuilt fingerprint
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I3b478bfac289d42c81dd0f1f2fb5f13db7e7813f

--- device.mk
+++ device.mk
@@ -199,7 +199,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
+    android.hardware.biometrics.fingerprint@2.1.vendor:64
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
