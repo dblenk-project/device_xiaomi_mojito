909da4e4c76f364ad65dbc184d0204e95682c8f2
Michael Bestas <mkbestas@lineageos.org>
2022-10-30 19:01:36 +0200

    mojito: Move qti_whitelist.xml to /system_ext
    Change-Id: Id8617513cfd686b3f6888a89c1b1db196acfb476
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>
    Signed-off-by: kibria5 <mdkibria687@gmail.com>

--- device.mk
+++ device.mk
@@ -384,7 +384,7 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext_privapp-permissions-qti.xml
 
 # RIL
