ffaa453e9f13d861427aac07651e2cd115d1f3db
johnmart19 <johnivan19999@gmail.com>
2022-11-03 21:40:13 +0200

    mojito: Enable ART Flags for A/B as per Google Recommendations
    Change-Id: Ic41b4b9d07e490fb3ca7810250bc451890230f28

--- device.mk
+++ device.mk
@@ -366,6 +366,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock
 
+# Preopted ODEX files (system_other)
+PRODUCT_PACKAGES += \
+    cppreopts.sh
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
