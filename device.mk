101a4cdba56d5984877163dc201a14bee56d4953
johnmart19 <johnivan19999@gmail.com>
2023-01-09 21:23:53 +0200

    mojito: Audio: Enable audio adsprpcd daemon
    Enable compilation of audio adsprpcd daemon.
    
    Small info: This is part of VoidUI-Tiramisu/hardware_qcom-caf_sm8250_audio@04baa1217e4ab263d69cc6af8e06c0ee7be456ee
    
    - We have "libadsp_default_listener.so" library and other dependant hals for this environment
    - Sepolicy for this we are having by default PixelExperience/device_qcom_sepolicy_vndr@03962670ef0ab32312bdc7346f146d2ac5e8017b
    
    Test:
    
    Targets compiled:
    * vendor/bin/audioadsprpcd
    * vendor/etc/init/vendor.qti.audio-adsprpc-service.rc
    
    Device booted fine to system, audio is working as usual.
    
    Reference of this commit: VoidUI-Tiramisu/hardware_qcom-caf_sm8250_audio@fb56879b933c0a1668c0c1d0beacf801cba6a511
    
    Co-authored-by: George Gao <georgao@codeaurora.org>
    Change-Id: Idd650989ab0ce989bcd3568cb59a3faee6d3259d

--- device.mk
+++ device.mk
@@ -83,6 +83,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Audio Daemon
+PRODUCT_PACKAGES += \
+    audioadsprpcd \
+    vendor.qti.audio-adsprpc-service.rc
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
