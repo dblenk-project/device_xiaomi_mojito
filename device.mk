1801e3300d535abb940ab50c38269bc3348f411e
boedhack99 <boedhack@gmail.com>
2023-01-21 11:30:28 +0700

    mojito: camera: Switch to PRODUCT_COPY_FILES * Also move folder to configs section
    Change-Id: I3f73d1e38d06be20c0d6e9ee1d35e57ab4449671

--- device.mk
+++ device.mk
@@ -124,8 +124,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libpiex_shim \
-    camxoverridesettings.txt
+    libpiex_shim
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
