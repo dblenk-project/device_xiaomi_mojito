707eb5151137e1ec27522b58fc22f3349bea53bb
boedhack99 <boedhack@gmail.com>
2023-04-21 05:25:55 +0700

    mojito: Remove power mode extentions
    Change-Id: If10118da9acec699efb17c6e0f94f78289d1ea13

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,7 +175,6 @@ TARGET_BOARD_PLATFORM := sm6150
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/props/odm.prop
