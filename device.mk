d78934ae03b071266a1114f70041851dffab2cd5
AmeChanRain <downloadbot007@gmail.com>
2023-10-05 12:26:45 +0000

    mojito: Ship with prebuilt libprotobuf-cpp-full
    * libprotobuf-cpp-full-vendorcompat and libprotobuf-cpp-lite-vendorcompat are deprecated
    
    Change-Id: I2383af8bba6560bae28cd609e0dd791e94136506
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>

--- device.mk
+++ device.mk
@@ -401,6 +401,10 @@ PRODUCT_PACKAGES += \
     librilutils \
     librmnetctl
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v30/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
