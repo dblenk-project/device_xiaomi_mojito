3b143af1265380643dc397a776073c955446e99b
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    mojito: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da
    Signed-off-by: Fiqri Ardyansyah <fiqri0927936@gmail.com>

--- device.mk
+++ device.mk
@@ -378,6 +378,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     cppreopts.sh
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
