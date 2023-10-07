0b18f2b586a103f36ad8854ccc597d941a97b3aa
basamaryan <basam.aryan@gmail.com>
2023-10-07 15:17:04 -0400

    mojito: Build libprotobuf-cpp-full-3.9.1-vendorcompat
    For now this is implicitly built by lineage specific
    libprotobuf-cpp-lite-3.9.1-vendorcompat lib, but in the future this
    might not be the case.
    
    Change-Id: I724e933e046cbf631c078321d55550b96ca998da

--- device.mk
+++ device.mk
@@ -398,14 +398,12 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
     libjson \
+    libprotobuf-cpp-full \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
     libril \
     librilutils \
     librmnetctl
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v30/arm/arch-arm-armv7-a-neon/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
-    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so
 
 # Sensors
 PRODUCT_PACKAGES += \
