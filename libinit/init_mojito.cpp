14c83adaddb647816a65f0726913da086ae74535
boedhack99 <boedhack@gmail.com>
2023-11-04 23:27:08 +0700

    mojito: Update to mojito 12 MIUI V14.0.8.0.SKGMIXM
    Change-Id: I79bf0d877dee371a268175d26da3840045665694

--- libinit/init_mojito.cpp
+++ libinit/init_mojito.cpp
@@ -77,17 +77,17 @@ void load_vendor_props() {
     if (hwname.find("sunny") != std::string::npos) {
         property_override("ro.product.device", "sunny");
         property_override("ro.product.name", "sunny");
-        property_override("ro.build.fingerprint", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V14.0.2.0.SKGMIXM:user/release-keys");
-        property_override("ro.vendor.build.fingerprint", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V14.0.2.0.SKGMIXM:user/release-keys");
-        property_override("ro.bootimage.build.fingerprint", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V14.0.2.0.SKGMIXM:user/release-keys");
-        property_override("ro.build.description", "sunny_global-user 12 SKQ1.210908.001 V14.0.2.0.SKGMIXM release-keys");
+        property_override("ro.build.fingerprint", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V14.0.8.0.SKGMIXM:user/release-keys");
+        property_override("ro.vendor.build.fingerprint", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V14.0.8.0.SKGMIXM:user/release-keys");
+        property_override("ro.bootimage.build.fingerprint", "Redmi/sunny_global/sunny:12/RKQ1.210614.002/V14.0.8.0.SKGMIXM:user/release-keys");
+        property_override("ro.build.description", "sunny_global-user 12 SKQ1.210908.001 V14.0.8.0.SKGMIXM release-keys");
     } else {
         property_override("ro.product.device", "mojito");
         property_override("ro.product.name", "mojito");
-        property_override("ro.build.fingerprint", "Redmi/mojito/mojito:12/RKQ1.210614.002/V14.0.2.0.SKGMIXM:user/release-keys");
-        property_override("ro.vendor.build.fingerprint", "Redmi/mojito/mojito:12/RKQ1.210614.002/V14.0.2.0.SKGMIXM:user/release-keys");
-        property_override("ro.bootimage.build.fingerprint", "Redmi/mojito/mojito:12/RKQ1.210614.002/V14.0.2.0.SKGMIXM:user/release-keys");
-        property_override("ro.build.description", "mojito-user 12 SKQ1.210908.001 V14.0.2.0.SKGMIXM release-keys");
+        property_override("ro.build.fingerprint", "Redmi/mojito/mojito:12/RKQ1.210614.002/V14.0.8.0.SKGMIXM:user/release-keys");
+        property_override("ro.vendor.build.fingerprint", "Redmi/mojito/mojito:12/RKQ1.210614.002/V14.0.8.0.SKGMIXM:user/release-keys");
+        property_override("ro.bootimage.build.fingerprint", "Redmi/mojito/mojito:12/RKQ1.210614.002/V14.0.8.0.SKGMIXM:user/release-keys");
+        property_override("ro.build.description", "mojito-user 12 SKQ1.210908.001 V14.0.8.0.SKGMIXM release-keys");
     }
 
     property_override("bluetooth.device.default_name", "Redmi Note 10");
