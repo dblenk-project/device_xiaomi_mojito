c5db14bd1b3eb26a7cefbacae20b94f0326b3ca0
boedhack99 <boedhack@gmail.com>
2023-11-05 22:13:12 +0700

    mojito: Don't build Power Stas HAL
    Change-Id: I4b4e27208c0b3cf2db68b34746fefa747374e424

--- device.mk
+++ device.mk
@@ -375,8 +375,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power-service-qti
 
 # Preopted ODEX files (system_other)
 PRODUCT_PACKAGES += \
